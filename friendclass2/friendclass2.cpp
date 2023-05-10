7b7a7b620c73d7215cdd90355f0e39988afbd3fb
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-10 16:08:17 +0700

    proses utama untuk menampilkan setNama

--- friendclass2/friendclass2.cpp
+++ friendclass2/friendclass2.cpp
@@ -12,3 +12,12 @@ void setNama(mahasiswa& a, string pNama) {
 	a.nama = pNama;
 	cout << "Nama: " << a.nama;
 }
+
+int main()
+{
+	mahasiswa mhs;
+	setNama(mhs, "Joko Kumat");
+	cout << endl;
+	system("pause");
+	return 0;
+}
